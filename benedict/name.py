ab71a0bbe83eee6c2642a8691557d296c62f2855
slurpyb <slurpyb@protonmail.com>
2022-04-22 15:19:56 +0800

    added new words and changed grammar

--- benedict/name.py
+++ benedict/name.py
@@ -7,7 +7,15 @@ class Name():
         self.closed_list = parse_word_list("benedict/data/closed_first.txt")
         self.open_list = parse_word_list("benedict/data/open_first.txt")
         self.vowel_list = parse_word_list("benedict/data/vowels.txt")
-    
+        self.cle_list = parse_word_list("benedict/data/cle_words.txt")
+        self.endings_list = parse_word_list("benedict/data/endings.txt")
+        self.three_letter_words_list = parse_word_list("benedict/data/three_letter_words.txt")
+        self.four_letter_words_list = parse_word_list("benedict/data/four_letter_words.txt")
+        self.five_letter_words_list = parse_word_list("benedict/data/five_letter_words.txt")
+        self.rcon_words_list = parse_word_list("benedict/data/rcon_words.txt")
+
     def __str__(self):
-        benedict_string = random_item(self.open_list) + random_item(self.vowel_list) + ' ' + random_item(self.closed_list) + random_item(self.closed_list)
+        structure = [self.cle_list, self.four_letter_words_list, self.three_letter_words_list, self.rcon_words_list]
+        # benedict_string = random_item(self.open_list) + random_item(self.vowel_list) + ' ' + random_item(self.closed_list) + random_item(self.closed_list)
+        benedict_string = random_item(structure[0]) + random_item(structure[1]) + ' ' + random_item(structure[2]) + random_item(structure[3])
         return benedict_string.title()
